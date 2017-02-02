131d022f607812bb6b3c71442fab201296b8a992
Jie He <sj-jie@sjtu.edu.cn>
2017-02-02 21:42:08 +0100

    Update BFS_and_A_star_9-puzzle_game.py

--- BFS_and_A_star_9-puzzle_game.py
+++ BFS_and_A_star_9-puzzle_game.py
@@ -10,12 +10,12 @@ def BFS_jeu_Taquin(e_init, e_sol, graph=None, get_graph = False):
 	if not get_graph: 
 		check_resolvable(e_init, graph)
 		if e_init == e_sol: 
-			print 'BFS finds solution of 0 step(s) when visited 0 states.'
+			print('BFS finds solution of 0 step(s) when visited 0 states.')
 			return
-		print '\nStarting BFS search...'
+		print('\nStarting BFS search...')
 	if get_graph: 
 		e_init = '123456789'
-		print 'Starting graph generation...'
+		print('Starting graph generation...')
 	store_graph = {}
 	queue = []
 	pred = {}
@@ -35,23 +35,23 @@ def BFS_jeu_Taquin(e_init, e_sol, graph=None, get_graph = False):
 			if (not get_graph) and (c in E_star): 
 				end = True
 				reconstruct_path(pred, c, 'BFS', count)
-			if count%20000 == 0: print 'States visited: %d\t...' % count
+			if count%20000 == 0: print('States visited: %d\t...' % count)
 			count += 1
 	if get_graph: 
 		fname = 'graph_Taquin_even.txt'
 		json.dump(store_graph, open(fname,'w'),indent=1)
-		print 'Graph generated and stored in %s' % fname
+		print('Graph generated and stored in %s' % fname)
 
 def A_star_Taquin(start, goal, graph):
 	check_resolvable(start, graph)
-	print '\nStarting A-star search...'
+	print('\nStarting A-star search...')
 	processed = set()
 	ongoing = set ()
 	ongoing.add(start)
 	fCost = {}
 	lowestCostTo = {}
 	lowestCostTo[start] = 0
-	fCost[start] = h_Taquin(start, goal)
+	fCost[start] = heuristic_Taquin(start, goal)
 	bestPrevious = {}
 	count = 0
 	end = False
@@ -65,15 +65,16 @@ def A_star_Taquin(start, goal, graph):
 		for n in unvisited_transmissions(bestNode, processed, graph)[0]:
 			new_cost_to_n = lowestCostTo[bestNode] + 1 # cost(current, next) = 1
 			if n not in ongoing: ongoing.add(n)
-			elif new_cost_to_n >= lowestCostTo[n]: continue # use 'else if' to assure that the n exists
+			elif new_cost_to_n >= lowestCostTo[n]: continue # use 'elif' to assure that the n exists
 			lowestCostTo[n] = new_cost_to_n
 			bestPrevious[n] = bestNode
-			fCost[n] = lowestCostTo[n] + h_Taquin(n, goal)
-			if count%5000==0: print 'States visited: %d\t...' % count
+			fCost[n] = lowestCostTo[n] + heuristic_Taquin(n, goal)
+			if count%5000==0: print('States visited: %d\t...' % count)
 			count += 1
 
 
-def unvisited_transmissions(e, visited, graph=None, get_graph=False): # return the next possible and unvisited states
+def unvisited_transmissions(e, visited, graph=None, get_graph=False): 
+	''' return the next possible and unvisited states '''
 	store_etats = [] # get all new states even some of them are already visited
 	new_etats = [] # possible and unvisited transmissions
 	if get_graph:
@@ -96,7 +97,7 @@ def unvisited_transmissions(e, visited, graph=None, get_graph=False): # return t
 			if el not in visited: new_etats.append(el)
 	return new_etats, store_etats
 
-def h_Taquin(current, e_sol): # get heuristic by the sum of Manhattan distance
+def heuristic_Taquin(current, e_sol): # get heuristic by the sum of Manhattan distance
 	sum_distance = 0
 	for v in e_sol:
 		p_sol = divmod(e_sol.index(v), 3)
@@ -110,47 +111,41 @@ def reconstruct_path(cameFrom, n, name, count): # return the shortest path to th
 		n = cameFrom[n]
 		total_path.append(n)
 	total_path.reverse()
-	print '%s finds solution of %s step(s) when visited %d states: %s\n' % (name, len(total_path)-1, count, ' -> '.join(total_path))
+	print('%s finds solution of %s step(s) when visited %d states: %s\n' % (name, len(total_path)-1, count, ' -> '.join(total_path)))
 	
 def check_resolvable(e_init, graph):
 	if e_init not in graph:
-		print '\nCan not find solution for the state: %s. Please choose another state to start! \n' % e_init
-		print '\nEnd of the project <<<<<<'
+		print('\nCan not find solution for the state: %s. Please choose another state to start! \n' % e_init)
+		print('\nEnd of the project <<<<<<')
 		exit()
 
 
 
 def main():
 	parser = argparse.ArgumentParser(description='A script to find the best solution of the game Taquin by applying two search algorithms on its state graph: BFS and A-star. The states are represented by strings such as \'123456789\'. The goal state is defined as 123456789 where 9 is the empty square.')
-	parser.add_argument('-g', action='store_true', help='generate and store the state graph of Taquin')
+	parser.add_argument('-g', action='store_true', help='generate and store the state graph of Taquin, not necessary if the graph is already generated')
 	parser.add_argument('-i', type=str, help='initial state (a random sequence will be generated if not given)')
 	parser.add_argument('-a', action='store_true', help='apply A-star search algorithm')
 	parser.add_argument('-b', action='store_true', help='apply BFS search algorithm')
 	parsed = parser.parse_args()
 	if not any(vars(parsed).values()): parser.error("At least one of -a or -b must be given")
 
-
+	e_sol = '123456789' # the goal state
+	Taquin_init = [i for i in range(1,10)] # 9 is the blank square
 	if parsed.g: BFS_jeu_Taquin(Taquin_init, e_sol, get_graph=True) # generate the graph
 
 	try: # load the graph as dictionary
 		graph = json.load(open("graph_Taquin_even.txt")) 
-		print 'Number of states in Taquin graph: %d' % len(graph)
+		print('Number of states in Taquin graph: %d' % len(graph))
 	except:
-		print 'No state graph file found, please add \'-g\' to generate the graph.'
+		print('No state graph file found, please add \'-g\' to generate the graph.')
 
-	e_sol = '123456789' # the goal state
 	if not parsed.i:
-		Taquin_init = [i for i in xrange(1,10)] # 9 is the blank square
 		shuffle(Taquin_init) # generate a random initial state
 		Taquin_init = ''.join(map(str,Taquin_init)) # convert it to String type
 	else: Taquin_init = parsed.i# set a manual initial state
 
-	print 'Initial Taquin state: %s %s' % (Taquin_init, '(randomly generated)' if not parsed.i else ' ')
-
-
-	
-
-
+	print('Initial Taquin state: %s %s' % (Taquin_init, '(randomly generated)' if not parsed.i else ' '))
 
 	if parsed.a: A_star_Taquin(Taquin_init, e_sol, graph) # start A-star search
 
